e42c138f0c64b565df46206a1f5c47f59ead5556
Stas <staspanasuk@mail.ru>
2018-02-24 12:46:08 +0200

    Initial Commit

---
+++ Photo Overlayer/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  DocumentBrowserViewController.swift
+//  Photo Overlayer
+//
+//  Created by Stas Panasuk on 1/31/18.
+//  Copyright © 2018 iSan4eZ. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+	var window: UIWindow?
+}
